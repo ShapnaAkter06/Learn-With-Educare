9e83f12afc7d7ec905d23f019e844dff9ee0a0a0
Shapna <shapna088@gmail.com>
2022-10-11 06:42:25 +0600

    option added

--- src/Components/Question/Question.js
+++ src/Components/Question/Question.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
-const Question = ({quiz}) => {
+const Question = ({ quiz }) => {
     console.log(quiz);
-    const {question, options} = quiz
+    const { question, options } = quiz
     return (
-        <div className='bg-gray-200'>
+        <div className='bg-gray-200 shadow-lg p-4'>
             <h2>Question : {question} </h2>
-            <p>Option:{options} </p>
+            <div className='grid md:grid-cols-1'>
+                {
+                    options.map(option => <label>
+                        <input type="checkbox" name="" id="" />
+                        {option}</label>)
+                }
+            </div>
         </div>
     );
 };
