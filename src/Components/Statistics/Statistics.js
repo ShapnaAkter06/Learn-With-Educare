992b57b5d755cb4111c8d49b8a45f7ce94da84f5
Shapna <shapna088@gmail.com>
2022-10-12 22:17:33 +0600

    changed statistics design

--- src/Components/Statistics/Statistics.js
+++ src/Components/Statistics/Statistics.js
@@ -1,20 +1,37 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
-import { LineChart, Line, XAxis, YAxis, Tooltip, Legend } from 'recharts';
-
+import { Area, AreaChart, CartesianGrid, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
 
 const Statistics = () => {
     const data = useLoaderData();
     // console.log(totalChart);
     return (
-        <div className='bg-white flex items-center justify-center'>
-        <LineChart width={350} height={400} data={data.data}>
-            <Line type="monotone" dataKey="total" stroke="#82ca9d" />
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Tooltip />
-            <Legend />
-        </LineChart>
+        <div className="h-screen bg-white flex items-center justify-center w-full mt-16">
+            <ResponsiveContainer width="100%" height="100%">
+                <AreaChart
+                    width={500}
+                    height={400}
+                    data={data.data}
+                    margin={{
+                        top: 10,
+                        right: 30,
+                        left: 0,
+                        bottom: 0,
+                    }}
+                >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <Tooltip />
+                    <Area
+                        type="monotone"
+                        dataKey="total"
+                        stroke="#8884d8"
+                        fill="#8884d8"
+                    />
+
+                </AreaChart>
+            </ResponsiveContainer>
         </div>
     );
 };
